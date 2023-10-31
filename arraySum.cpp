25efd9d3fc489291335bce3797543e187e05aeac
Shambhavi Singh <115347011+SinSham@users.noreply.github.com>
2023-10-31 16:19:03 +0530

    Create arraySum.cpp

---
+++ arraySum.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int findSum(int arr[], int n){
+
+    if(n==2){
+        return arr[0] + arr[1];
+    }
+    
+    int sum = arr[0] + findSum(arr+1, n-1);
+    
+    return sum;
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+    int* arr = new int[n];
+    for(int i=0; i<n; i++){
+        cin>>arr[i];
+    }
+    int sum = findSum(arr, n);
+    cout<<sum<<endl;
+
+    return 0;
+}
